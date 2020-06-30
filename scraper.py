e4e210aba07ff0eeaf6c19059f5b328dc6d00f4e
Oleg Lavrovsky <oleg@utou.ch>
2020-06-30 10:01:59 +0200

    Added scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,17 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests, scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# JSON file provided by the Swiss Federal Office of Statistics
+# Source: https://www.experimental.bfs.admin.ch/expstat/en/home/innovative-methods/swisscovid-app-monitoring.assetdetail.13428386.html
+url = "https://www.bfs.admin.ch/bfsstatic/dam/assets/orderNr:ds-q-14.01-SwissCovidApp-01/master"
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+r = requests.get(url) #, verify=False)
+
+if r.status_code != 200:
+    print('Error', r.status_code, r.text)
+    exit()
+
+data = r.json()
+print('Updating', data['dataStatus'])
+
+# Save to database
+scraperwiki.sqlite.save(unique_keys=['Date'], data=data['data'])
